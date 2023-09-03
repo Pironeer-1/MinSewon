899b5c4f62483f963a1f43d059b71f850f66c90c
Seown Min <nvpz1598@gamil.com>
2023-09-03 18:34:22 +0900

    2-2. JSX 구문 탐구하기

---
+++ ch02/ch02_2/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {SafeAreaView, Text} from 'react-native'
+
+export default function App() {
+  const children = new Array(100)
+    .fill(null)
+    .map((notUsed, index) => <Text>Hello Wolrd! {index}</Text>)
+
+  return <SafeAreaView>{children}</SafeAreaView>
+}
