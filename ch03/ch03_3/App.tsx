d1bf8721424e5132995897ddde4801d1528ff21f
Seown Min <nvpz1598@gamil.com>
2023-09-05 03:59:46 +0900

    3.3 자원과 아이콘 사용하기

---
+++ ch03/ch03_3/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {StyleSheet, SafeAreaView, ImageBackground, Image} from 'react-native'
+import * as D from './src/data'
+
+const avatarUrl = D.ramdomAvatarUrl()
+const avatarSize = 50
+
+export default function App() {
+  return (
+  <SafeAreaView style={[styles.flex]}>
+    <ImageBackground
+      style={[styles.flex, styles.ImageBackground]}
+      source={require('./src/assets/images/bg.jpg')}>
+      <Image source={{ uri: avatarUrl}} style={[styles.image]} />
+    </ImageBackground>
+  </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  flex: {flex: 1},
+  ImageBackground: {padding: 10},
+  image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize/2 }
+})
